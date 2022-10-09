46befd99fcb5b6b7f27909ed4fde26913661d6f1
Kuleen Sasse <56900136+KuleenS@users.noreply.github.com>
2022-10-09 15:29:10 -0400

    Update lorautils.h

--- lorautils/src/lorautils.h
+++ lorautils/src/lorautils.h
@@ -1,7 +1,7 @@
 #include <stdint.h>
 
-void cast_char_to_packet(uint8_t  * buffer, Packet * packet);
+void cast_char_to_packet(uint8_t  * buffer, void * packet);
 
-void cast_packet_to_char(Packet * packet, uint8_t  * buffer);
+void cast_packet_to_char(void * packet, uint8_t  * buffer);
 
 void cast_char_to_lora_data(char * data, uint8_t * buffer);
