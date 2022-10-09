005cfcff03609372da4b30327a8f7f5f69e7f51d
Kuleen Sasse <56900136+KuleenS@users.noreply.github.com>
2022-10-09 15:29:39 -0400

    Update lorautils.cpp

--- lorautils/src/lorautils.cpp
+++ lorautils/src/lorautils.cpp
@@ -1,10 +1,10 @@
 #include "lorautils.h"
 
-void cast_data_to_packet(uint8_t * buffer, Packet * packet){
-    packet = (Packet *) buffer;
+void cast_data_to_packet(uint8_t * buffer, void * packet){
+    packet = (void *) buffer;
 }
 
-void cast_packet_to_data(Packet * packet, uint8_t * buffer){
+void cast_packet_to_data(void * packet, uint8_t * buffer){
     buffer = (uint8_t *) packet;
 }
 
